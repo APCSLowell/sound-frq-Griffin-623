aeeae478410e29e03fe63bb2ffe2115f485c9ab5
Griffin-623 <79292572+Griffin-623@users.noreply.github.com>
2024-04-05 13:37:08 -0700

    Update Sound.java

--- src/main/java/Sound.java
+++ src/main/java/Sound.java
@@ -18,12 +18,13 @@ public class Sound
   {  
     int count = 0;
     for(int i =1;i<samples.length;i++){
-      if(samples[i]>limit||samples[i]<limit*-1){
-        count++;
-        if(samples[i]>limit){
+      if(samples[i]>limit){
           samples[i]=limit;
-        }else if(samples[i]<limit*-1){
+        count++;
+        }
+      if(samples[i]<limit*-1){
           samples[i]=limit*-1;
+        count++;
         }
       }
     }
